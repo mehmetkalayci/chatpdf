c4b2e84e4048382ba0354c3ab4b9870b68512a87
Mehmet <mmtkalayci@gmail.com>
2024-02-04 16:25:30 +0300

    initial commit

---
+++ src/lib/context.ts
@@ -0,0 +1,44 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+import { convertToAscii } from "./utils";
+import { getEmbeddings } from "./embeddings";
+
+export async function getMatchesFromEmbeddings(
+  embeddings: number[],
+  fileKey: string
+) {
+  const pinecone = new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY!,
+  });
+  const index = await pinecone.Index("chatpdf");
+
+  try {
+    const namespace = convertToAscii(fileKey);
+    const queryResult = await index.query({
+      topK: 5,
+      vector: embeddings,
+      includeMetadata: true,
+      id: namespace,
+    });
+    return queryResult.matches || [];
+  } catch (error) {
+    console.log("error querying embeddings", error);
+  }
+}
+
+export async function getContext(query: string, fileKey: string) {
+  const queryEmbeddings = await getEmbeddings(query);
+  const matches = await getMatchesFromEmbeddings(queryEmbeddings, fileKey);
+
+  const qualifyingDocs = matches?.filter(
+    (match) => match.score && match.score > 0.7
+  );
+
+  type Metadata = {
+    text: string;
+    pageNumber: number;
+  };
+
+  let docs = qualifyingDocs?.map((match) => (match.metadata as Metadata).text);
+
+  return docs?.join("\n").substring(0, 3000);
+}
